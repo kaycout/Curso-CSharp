215234987b6defeb3f134ea7335df7bf601b1484
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-10-10 15:54:51 -0300

    Exercícios de fixação em C#

---
+++ Curso C#/OperadoresAtribuicao/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,18 @@
+﻿// Operadores de atribuição
+
+// =, +=, -=, *=, /=, %=
+
+// Exemplos com esses operadores
+
+a = 10; // significa que, a váriavel a RECEBE 10.
+a += 2; // a váriavel a RECEBE a + 2; 
+// ou seja, o valor que já contia dentro da
+// váriavel, mais 2. 
+a -= 2; // a váriavel a RECEBE a - 2;
+// ou seja, o valor que já contia dentro da
+// váriavel, menos 2. 
+
+a *= 3; // Pegar o valor que já contem dentro da variavel
+// e multiplicar por 3.
+
+
---
+++ Curso C#/SaidaDados2/SaidaDados2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Saída de dados em C#
+
+using System;
+
+//namespace para imprimir números com pontos para separação de casas.
+using System.Globalization;
+
+namespace SegundoExercicio
+{
+    class Exercicio
+    {
+        static void Main(string[] args)
+        {
+
+            // Declarando variáveis
+            char genero = 'F';
+            int idade = 18;
+            double saldo = 10.38844;
+            string nome = "Maria";
+
+            Console.WriteLine("Primeiros programas em C#");
+            Console.WriteLine("Aprendendo passo a passo os comandos em C#");
+            Console.WriteLine("Espero muito consegue aprender com esses exercícios");
+            Console.WriteLine("----------------------------------------------------------------");
+
+           // Chamando as variáveis
+           Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+
+            //Para imprimir somente 2 casas decimais da variável SALDO.
+            Console.WriteLine(saldo.ToString("F2"));
+
+            //Para imprimir com o . separador
+            Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Diferença entre Write e WriteLine
+            // Write imprime a mensagem no terminal e não quebra linha.
+            // WriteLine imprime a mensagem no terminal, quebra a linha
+            // e escreve a mensagem logo abaixo. 
+
+        }
+    }
+}
