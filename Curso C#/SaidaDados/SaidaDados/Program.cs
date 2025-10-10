3cfb6f683388b9e8ae83ecd4b517db45d661b1b2
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-10-10 15:49:42 -0300

    Exercícios de fixação em C#

---
+++ Curso C#/SaidaDados/SaidaDados/Program.cs
@@ -0,0 +1,24 @@
+﻿// Diferença de PLaceholders, concatenação e interpolação.
+
+using System;
+using System.Globalization;
+
+int idade = 18;
+double saldo = 50.2345;
+string nome = "Maria";
+
+// Para imprimir as informações que
+// contém dentro das variáveis em uma única frase
+// com Placeholder.
+
+Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+// {2:F2} - Para separar a quantidade de casas decimais.
+
+// Para imprimir informações com Interpolação.
+
+Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+// Para imprimir informações com Concatenação.
+
+Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
