9e71b6e9f303ad26327f743526256d6db9a9a700
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-10-16 12:22:03 -0300

    Exercícios em C#

---
+++ Curso C#/ExecFixacao1/ExecFixacao1/Program.cs
@@ -0,0 +1,49 @@
+﻿//Programa em C# - Primeiro exercício de Fixação.
+
+// foi feito o debug e funcionou
+
+using System;
+using System.Globalization;
+
+namespace PrimeiroExercicio
+{
+    class Exercicio
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com seu nome Completo:");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double produto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre seu último nome, idade e altura");
+            string[] frase = Console.ReadLine().Split(' ');
+            
+            
+            string ultimoNome = frase[0];
+            int idade = int.Parse(frase[1]);
+            double altura = double.Parse(frase[2], CultureInfo.InvariantCulture);
+
+            //chamando as variáveis declaradas.
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartos);
+            Console.WriteLine(produto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+
+
+
+
---
+++ Curso C#/ExecFixacao2/ExecFixacao2/Program.cs
@@ -0,0 +1,50 @@
+﻿//Programa em C# - Segundo Exercício de Fixação.
+
+// feito o debug 
+
+using System;
+using System.Globalization;
+
+namespace SegundoExercicio
+{
+    class Exercicio2
+    {
+        static void Main(string[] args)
+        {
+
+            string produto1 = "Notebook";
+            string produto2 = "Escrivaninha";
+
+            byte idade = 30;
+            int codigoProduto = 5345;
+            char genero = 'M';
+
+            double preco1 = 3500.0;
+            double preco2 = 700.90;
+            double medida = 54.234565;
+
+            Console.WriteLine("Produtos:");
+            // Placeholder
+            // {1:F2} para diminuir casas decimais.
+            Console.WriteLine("{0}, cujo preço é $ {1:F2}", produto1, preco1);
+            Console.WriteLine("{0}, cujo preço é $ {1:F2}", produto2, preco2);
+
+            // Concatenação
+            Console.WriteLine("Registro:");
+            Console.WriteLine(idade + " anos de idade, código " + codigoProduto + " e gênero: " + genero);
+
+            // Para imprimir a variavel "medida" com 8 casas decimais.
+
+            Console.WriteLine("Medidas com oito casa decimais:");
+            Console.WriteLine(medida.ToString("F8"));
+
+            // Para imprimir apenas com 3 casa decimais.
+            Console.WriteLine("Arredondado (três casas decimais):");
+            Console.WriteLine(medida.ToString("F3"));
+
+            // Para imprimir separando as casas decimais com "."
+            Console.WriteLine("Separador decimal invariant culture:");
+            Console.WriteLine(medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
