0ba25c699ed842e29144a25c2583be2684d20e74
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-11-04 14:49:35 -0300

    Exercícios de fixação em C#

---
+++ Curso C#/ExecFixacao9/ExecFixacao9/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace ExecFixacao9
+{
+    internal class ContaBancaria
+    {
+        public int iNumero {  get; private set; }
+        public string sTitular { get; set; }
+        public double dSaldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            iNumero = numero;
+            sTitular = titular;
+        }
+
+        public ContaBancaria( int numero, string titular, double depositoInicial ) : this (numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito (double dQuantia)
+        {
+            dSaldo += dQuantia;
+        }
+
+        //void porque não retorna nada. 
+        //para sacar dinheiro tem uma taxa de R$ 5,00.
+        public void Saque (double dQuantia)
+        {
+            dSaldo -= dQuantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + iNumero
+                + ", Titular: "
+                + sTitular
+                + ", Saldo: $ "
+                + dSaldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Curso C#/ExecFixacao9/ExecFixacao9/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+// Fazer um programa onde em um banco para se cadastrar é
+// necessário informar o número da conta, nome do titular da conta
+// e o valor de depósito inicial que o titular depositou ao abrir
+// a conta.
+
+
+using System;
+using ExecFixacao9;
+
+namespace Exercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int iNumero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string sTitular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            char sResp = char.Parse(Console.ReadLine());
+            if (sResp == 's' || sResp == 'S' )
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double dDepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(iNumero, sTitular, dDepositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(iNumero, sTitular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double dQuantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(dQuantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            dQuantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(dQuantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
