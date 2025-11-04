fcecb0626699e59ce62a30d3b7ce915691e550a5
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-11-04 14:48:11 -0300

    Exercícios de fixação em C#

---
+++ Curso C#/ExecFixacao7/ExecFixacao7/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace ExecFixacao7
+{
+    internal class Produto
+    {
+        public string sNome;
+        public double dPreco;
+        public double iQuantidade;
+
+        public Produto(string nome, double preco, double quantidade)
+        {
+            sNome = nome;
+            dPreco = preco;
+            iQuantidade = quantidade;
+        }
+
+        public double dValorTotalEmEstoque()
+        {
+            return dPreco * iQuantidade;
+
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            iQuantidade = iQuantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            iQuantidade = iQuantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return sNome
+                + ", $ "
+                + dPreco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + iQuantidade
+                + " unidades, Total: $ "
+                + dValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Curso C#/ExecFixacao7/ExecFixacao7/Program.cs
@@ -0,0 +1,49 @@
+﻿// Exercício Construtor
+
+// Construtor é uma operação de classe que executa o comando
+// no momento da instanciação do objeto.
+
+// Inicia os valores dos atributos
+// permite ou obriga o objeto a receber dados
+
+using ExecFixacao7;
+using System.Globalization;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string sNome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double dPreco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int iQuantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(sNome, dPreco, iQuantidade);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int iQntd = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(iQntd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a serem removidos do estoque: ");
+            iQntd = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(iQntd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            
+        }
+    }
+}
