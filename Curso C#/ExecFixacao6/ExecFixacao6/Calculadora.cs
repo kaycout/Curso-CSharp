8e2b7dbbca643f541bd4e03c968bb9633a16282b
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-11-04 14:47:12 -0300

    Exercícios de fixação em C#

---
+++ Curso C#/ExecFixacao6/ExecFixacao6/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿namespace ExecFixacao6
+{
+    internal class Calculadora
+    {
+         public static double dPi = 3.14;
+
+        public static double Circunferencia(double dR)
+        {
+            return 2.0 * dPi * dR;
+        }
+
+        public static double Volume(double dR)
+        {
+            return 4.0 / 3.0 * double.Pi * dR * dR * dR;
+
+        }
+    }
+}
---
+++ Curso C#/ExecFixacao6/ExecFixacao6/Program.cs
@@ -0,0 +1,37 @@
+﻿// Membros estáticos 
+
+// chamados de membros de "Classe"
+
+// São membros que fazem sentido independentemente 
+// de objetos e nao precisam de objeto para serem chamados. Pois,
+// são chamados pelo próprio nome da classe.
+
+
+// Fazer um programa para ler um valor numerico e mostrar
+// quanto seria o valor de uma circunferencia, volume e raio. Além de informar o
+// valor de PI com duas casas decimais.
+
+using ExecFixacao6;
+using System.Globalization;
+
+namespace Exercicio6
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre o valor do raio: ");
+            double dRaio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(dRaio);
+            double vol = Calculadora.Volume(dRaio);
+
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.dPi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
