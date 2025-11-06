40a554ece76d6a2cc40432f61850f2ee38c64a90
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-11-06 10:34:33 -0300

    Novos exercícios em C#
    Curso Udemy para Estágio

---
+++ ExecFixacao10/ExecFixacao10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
+
