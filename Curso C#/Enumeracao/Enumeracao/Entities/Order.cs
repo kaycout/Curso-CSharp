0cd31de66577e3c0bc24c1c7e83a0f437e724dfa
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-11-06 11:43:34 -0300

    Exercício de Enumeracao Csharp
    Cursinho Udemy Estágio

---
+++ Curso C#/Enumeracao/Enumeracao/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Enumeracao.Entities.Enums;
+
+namespace Enumeracao.Entities
+{
+    internal class Order
+    {
+        public int iId { get; set; }
+        public DateTime dtMoment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return iId
+                + ", "
+                + dtMoment
+                + ", "
+                + Status;
+                
+        }
+    }
+}
---
+++ Curso C#/Enumeracao/Enumeracao/Program.cs
@@ -0,0 +1,28 @@
+﻿// Exercício de Enumeracao.
+
+using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                iId = 200,
+                dtMoment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse < OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
