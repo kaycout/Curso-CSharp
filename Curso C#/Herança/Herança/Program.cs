40b8d06ec31ce34c66e403f44bdd7a15c225d8da
Kaylane Coutinho <149087078+kaycout@users.noreply.github.com>
2025-11-07 11:57:52 -0300

    Novos exercícios em C#
    Curso CSharp da Udemy para Estágio.

---
+++ Curso C#/Herança/Herança/Program.cs
@@ -0,0 +1,14 @@
+﻿// Heranca trata-se de um conceito em C# que permite que uma
+// classe herde dados e comportamentos de outra.
+
+// Como assim? Significa que ao inves de criar duas classes,
+// repetindo os mesmos atributos e metodos, pode-se criar apenas
+// uma classe e referenciar o metodo a outra classe, herdando
+// informacoes. 
+
+// isso significa que se criar outro metodo, ele pode conter 
+// todos os atributos de uma outra classe, mais as informações
+// que forem adicionadas.
+
+// em CSharp para fazer heranca. é necessario usar :.
+
